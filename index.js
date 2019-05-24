a5d7f7c8e40aab07f1693a20c662e37211bea2e3
naveen kumar sharma <37363386+NAVEEN326327@users.noreply.github.com>
2019-05-24 21:24:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var numberOfDrumButtons = document.querySelector(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0; i<numberOfDrumButtons; i++)
 {
-    document.querySelector(".drum")[i].addEventListener("click", function() {
-alert("I got clicked");
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+     var audio = new Audio('top-1.mp3');
+     audio.play();
 });
 
 }
     
+var me={
+    name: "Naveen Kumar Sharma", 
+    age: 22,
+    qualification: btech,
+
+}
+alert(me.name);
